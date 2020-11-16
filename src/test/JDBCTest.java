9a66fb0c42a60aad19ff40f9a3cc3d6da40c3554
Stan Chen <stanleychen587@gmail.com>
2020-11-15 19:54:41 -0700

    played with preparestatement, poc working

---
+++ src/test/JDBCTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import controller.JDBC;
+
+class JDBCTest {
+    public static void main(String[] args) {
+        JDBC jdbc = new JDBC();
+        jdbc.connectDB("localhost:3306","mysql","testadmin", "passw0rd");
+        jdbc.selectWithWildCard("items","description_name","Wi%");
+    }
+}
