30a4723719b8bbd61c497d746c67948a00880b7d
Stan Chen <stanleychen587@gmail.com>
2020-11-18 20:35:26 -0700

    implemented a basic implementation of toJSON

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -1,2 +1,22 @@
-package test;public class CustomerTest {
+package test;
+
+import client.model.Customer;
+
+/**
+ * to see a code snippet of how it's done
+ * https://blog.codota.com/how-to-convert-a-java-object-into-a-json-string/
+ */
+public class CustomerTest {
+    public static void main(String[] args) {
+        Customer testCustomer = new Customer(01,"Jason",
+                "Lol",
+                "1234 Campus Drive",
+                "T9X5X4",
+                "468-854-6548",
+                "International");
+
+        testCustomer.toJSON();
+    }
+
+
 }
