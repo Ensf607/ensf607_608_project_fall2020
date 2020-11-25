a30d96b336c17f5a9936306d1dafd9a212df93ed
Stan Chen <stanleychen587@gmail.com>
2020-11-25 00:51:09 -0700

    removed unecessary lib cause jenkins build to fail

--- src/server/controller/Response.java
+++ src/server/controller/Response.java
@@ -1,6 +1,5 @@
 package server.controller;
 
-import client.controller.Client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
